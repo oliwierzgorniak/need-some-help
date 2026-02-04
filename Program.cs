6752ca7f79ea4e043de0e972483b389848fc0131
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2026-02-04 12:27:56 +0100

    fix: solving crashes when trying to navigate the page quickly by switching to scoped factory pattern DbContext

--- Program.cs
+++ Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddSignalR().AddAzureSignalR(options =>
 builder.Services.AddRadzenComponents();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure()), lifetime: ServiceLifetime.Scoped);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure()));
 
